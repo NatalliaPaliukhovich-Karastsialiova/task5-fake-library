4a74876f2bb2529c1706ec2cbd15700b2920d48e
Natallia Paliukhovich-Karastsialiova <n.paliukhovich98@gmail.com>
2025-07-28 17:26:17 +0500

    fix: random function

--- server/utils/random.js
+++ server/utils/random.js
@@ -1,7 +1,8 @@
 exports.times = (n, rng, fn) => {
   if (n < 0) throw new Error("The first argument cannot be negative.");
     return (arg) => {
-      for(let i = Math.floor(n); i--;) arg = fn(arg);
-        return rng() < n % 1 ? fn(arg) : arg;
+      for(let i = Math.round(n); i--;) arg = fn(arg);
+        if (n % 1 !== 0 && rng() < n % 1) arg = fn(arg);
+      return arg;
   };
 };
